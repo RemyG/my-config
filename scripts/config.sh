9e27f219727bbab174a40ef782e00d491018d17e
Gardette Remy <remy.gardette@ext.leroymerlin.fr>
2017-11-16 17:16:14 +0100

    Fix config file path

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,8 +11,8 @@ mkdir -p $newdir
 for file in $files; do
   echo "mv ~/.$file $olddir/"
   mv ~/.$file $olddir/
-  echo "cp $configdir/$file $newdir/.$file"
-  cp $configdir/$file $newdir/.$file
+  echo "cp $configdir/$file $newdir/$file"
+  cp $configdir/$file $newdir/$file
   echo "ln -s $newdir/$file ~/.$file"
   ln -s $newdir/$file ~/.$file
 done
